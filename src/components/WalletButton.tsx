2b5dae338e3401ad4af6a212dcd4d679905ff8c5
Nithin Mengani <110032619+Nithin-Varma@users.noreply.github.com>
2023-11-16 15:56:28 +0000

    changes in frontend till now

--- src/components/WalletButton.tsx
+++ src/components/WalletButton.tsx
@@ -2,14 +2,17 @@ import { Button, Input, InputGroup, InputLeftAddon, InputRightElement, Modal, Mo
 import { useState } from 'react';
 import { useHoldIc } from "@hold-ic/react"
 import { md5 } from 'js-md5';
+import { useToast, Spinner } from '@chakra-ui/react'
 
 function WalletButton() {
     const { holdIC, isConnected } = useHoldIc()
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const [loading, setLoading] = useState(false);
     console.log({ isConnected })
+    const toast = useToast();
     const connectActor = async () => {
         const print = await holdIC?.getPrinicpal()
-        console.log({ print })
+        console.log("nvfnivenivubekbr",{ print })
         // backend_backend.insert(BigInt(1234), {
         //     link: "https://www.google.com",
         //     user: print ?? new principalIdFromHex("")
@@ -17,11 +20,12 @@ function WalletButton() {
         const actor: any = await holdIC.getActor('backend_backend')
         console.log({ actor })
         const a = await actor?.getLink(BigInt(1234));
-        console.log("wrgrmfeergouegn",{ a })
+        console.log({ a })
     }
     const [link, setLink] = useState("")
     const [longLink, setLongLink] = useState("")
     const shorten = async () => {
+        setLoading(true);
         const actor: any = await holdIC.getActor('backend_backend')
         const hasher = md5.create()
         hasher.update(link)
@@ -45,6 +49,21 @@ function WalletButton() {
         console.log({ result })
 
     }
+    const handleConnect = async () => {
+        setLoading(true);
+        await holdIC.connect("Plug", () => {
+          console.log("connected");
+          setLoading(false);
+          toast({
+            title: 'Successfully Connected',
+            description: "Ready to use...",
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          });
+          onClose();
+        });
+       };
     return (
         <>{
             isConnected ? (
@@ -54,11 +73,18 @@ function WalletButton() {
 
                         holdIC.disconnect(() => console.log("disconnet"))
                         window.location.reload()
+                        toast({
+                            title: 'Disconnected',
+                            description: "Visit Again",
+                            status: 'info',
+                            duration: 9000,
+                            isClosable: true,
+                          })
                     }}>
                         Disconnect
                     </Button>
                     <Button onClick={async () => {
-                        console.log(holdIC.wallet, "wallllett")
+                        console.log(holdIC.wallet, "wallet")
                         await connectActor()
                     }}>
                         Get value
@@ -66,7 +92,7 @@ function WalletButton() {
                 </>
             ) : (
                 <Button onClick={() => {
-                    console.log(holdIC.wallet, "wallllett")
+                    console.log(holdIC.wallet, "wallet")
                     onOpen()
                 }}>
                     Wallet Connect
@@ -79,11 +105,20 @@ function WalletButton() {
 
                     </Input>
                     <InputGroup size='sm'>
-                        <InputLeftAddon children='https://lifeisshort.ton/' />
+                        <InputLeftAddon children='https://life-is-short-iota.vercel.app/' />
                         <Input placeholder='my short link' value={link} onChange={(e) => setLink(e.target.value)} />
                         <InputRightElement width='4.5rem'>
                             <Button h='1.75rem' size='sm' onClick={shorten}>
-                                Shorten!
+                            {loading ? 
+                            <Spinner
+                                thickness='1px'
+                                speed='0.65s'
+                                emptyColor='gray.200'
+                                color='blue.500'
+                                size='sm'
+                             /> : 
+                             'Shorten'
+                             }
                             </Button>
                         </InputRightElement>
                     </InputGroup>
@@ -98,16 +133,17 @@ function WalletButton() {
                     <ModalHeader>Modal Title</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                        <Button onClick={
-                            async () => {
-                                await holdIC.connect("Plug", () => {
-                                    console.log("connected")
-                                    alert("Done")
-                                    onClose()
-                                })
-                            }
-                        }>
-                            Plug Wallet Connect
+                        <Button onClick={handleConnect}>
+                        {loading ? 
+                            <Spinner
+                                thickness='4px'
+                                speed='0.65s'
+                                emptyColor='gray.200'
+                                color='blue.500'
+                                size='sm'
+                             /> : 
+                             'Plug Wallet Connect'
+                             }
                         </Button>
                     </ModalBody>
 
